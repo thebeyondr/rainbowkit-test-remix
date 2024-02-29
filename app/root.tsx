b2529dbb8769cd790f28a32ed30444f775fe0fb8
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-02-29 08:38:10 -0500

    feat(rainbow-kit): :sparkles: Add rainbow tings

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,23 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  ConnectButton,
+  getDefaultConfig,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { http, WagmiProvider } from "wagmi";
+
+import { mainnet, sepolia } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import {
+  injectedWallet,
+  metaMaskWallet,
+  rainbowWallet,
+  zerionWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -24,6 +41,41 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+const queryClient = new QueryClient();
+
 export default function App() {
-  return <Outlet />;
+  const config = getDefaultConfig({
+    ssr: true,
+    appName: "RainbowKit Remix Example",
+    projectId: "YOUR PROJECT ID",
+    chains: [sepolia, mainnet],
+    wallets: [
+      {
+        groupName: "Recommended",
+        wallets: [rainbowWallet, zerionWallet, metaMaskWallet, injectedWallet],
+      },
+    ],
+    transports: {
+      [sepolia.id]: http(),
+    },
+  });
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              padding: "12px",
+            }}
+          >
+            <ConnectButton />
+          </div>
+        </RainbowKitProvider>
+        <Outlet />
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
